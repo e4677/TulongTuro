ccaa9e8f46bcae97409a5ca64efd4e364c6a61e5
baddddddddd <eluxeofficial@gmail.com>
2025-05-04 10:16:08 +0800

    Include subject title in lesson page backend API

--- controllers/lessonController.js
+++ controllers/lessonController.js
@@ -141,8 +141,14 @@ export const renderLesson = async (req, res) => {
     });
   }
 
+  const subjectSlug = lesson.subject_slug;
+  const subjectTitle = subjectSlug
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+
   const content = marked(lesson.content);
-  res.render('lesson', { lesson, content });
+  res.render('lesson', { lesson, content, subjectTitle });
 };
 
 export const getLessonContent = async (req, res) => {
