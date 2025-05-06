98888ff8ef3c5c651f6464ad48eb773409d99c90
baddddddddd <eluxeofficial@gmail.com>
2025-05-06 08:01:49 +0800

    Save state of sidebar across pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,23 @@
 const toggleButton = document.getElementById("toggleButton");
 toggleButton.addEventListener("click", () => {
 	document.body.classList.toggle("sidebar-closed");
+
+	if (document.body.classList.contains("sidebar-closed")) {
+		localStorage.setItem('sidebar', 'closed');
+	} else {
+		localStorage.setItem('sidebar', 'open');
+	}
 });
+
+if (localStorage.getItem('sidebar') === 'closed') {	
+	document.body.style.transition = 'none';
+	document.body.style.offsetHeight;
+	document.body.classList.add("sidebar-closed");
+
+	setInterval(() => {
+		document.body.style.transition = '';
+	}, 600)
+	
+} else {
+	document.body.classList.remove("sidebar-closed");
+}
