bbfe4b30f5169013ad01e14526f68f58067387bd
mothy-08 <davetimothyy@gmail.com>
2025-05-06 13:27:29 +0800

    changes: chat display styles and animations

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,38 @@
 const toggleButton = document.getElementById("toggleButton");
 toggleButton.addEventListener("click", () => {
-	document.body.classList.toggle("sidebar-closed");
+  document.body.classList.toggle("sidebar-closed");
 
-	if (document.body.classList.contains("sidebar-closed")) {
-		localStorage.setItem('sidebar', 'closed');
-	} else {
-		localStorage.setItem('sidebar', 'open');
-	}
+  if (document.body.classList.contains("sidebar-closed")) {
+    localStorage.setItem("sidebar", "closed");
+  } else {
+    localStorage.setItem("sidebar", "open");
+  }
 });
 
-if (localStorage.getItem('sidebar') === 'closed') {	
-	document.body.style.transition = 'none';
-	document.body.style.offsetHeight;
-	document.body.classList.add("sidebar-closed");
+if (localStorage.getItem("sidebar") === "closed") {
+  document.body.style.transition = "none";
+  document.body.style.offsetHeight;
+  document.body.classList.add("sidebar-closed");
 
-	setInterval(() => {
-		document.body.style.transition = '';
-	}, 600)
-	
+  setInterval(() => {
+    document.body.style.transition = "";
+  }, 600);
 } else {
-	document.body.classList.remove("sidebar-closed");
+  document.body.classList.remove("sidebar-closed");
 }
+
+const floatingChatBubble = document.getElementById("floating-chat-bubble");
+const chatWrapper = document.getElementById("chat-wrapper");
+const closeButton = document.getElementById("close-button");
+
+floatingChatBubble.addEventListener("click", () => {
+  chatWrapper.style.transform = "translateX(0)";
+  chatWrapper.style.opacity = "1";
+  floatingChatBubble.style.display = "none";
+});
+
+closeButton.addEventListener("click", () => {
+  chatWrapper.style.transform = "translateX(100%)";
+  chatWrapper.style.opacity = "0";
+  floatingChatBubble.style.display = "flex";
+});
