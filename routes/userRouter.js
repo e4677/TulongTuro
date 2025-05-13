79c6de9a42416fc421040486f9245966ee17b173
baddddddddd <eluxeofficial@gmail.com>
2025-05-13 09:32:25 +0800

    Add logout functionality

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import * as userController from "../controllers/userController.js";
+import { logout } from "../config/jwtUtils.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/login", userController.renderLogin);
 userRouter.get("/signup", userController.renderSignup);
+userRouter.get("/logout", logout);
 
 userRouter.post("/api/login", userController.verifyLogin);
 userRouter.post("/api/signup", userController.registerUser);
